df26bb5018e5a0e96fd984512eb49a9c084d53c7
demanluandyuki <demanluandyuki@126.com>
2018-05-21 08:15:32 +0800

    Symptom:add first commit Solution: RootCause:N/A Note:N/A

---
+++ app/src/main/java/com/joyfulmath/googlecalculator/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.joyfulmath.googlecalculator
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+/**
+ * Created by deman.lu on 2018/1/1 0001.
+ *
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
